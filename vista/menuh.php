e080590b98492f76addf763315303352a10d19e3
joaquinS25 <mitroncito25@hotmail.com>
2025-11-02 19:06:31 -0500

    BOTON CERRAR SESSION

--- vista/menuh.php
+++ vista/menuh.php
@@ -37,7 +37,7 @@ session_start();
                 Configurar
             </a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+            <a class="dropdown-item" href="cerrar_session.php">
                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                 Salir
             </a>
